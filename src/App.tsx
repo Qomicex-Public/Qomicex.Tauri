ac9d734d457e355bafb94074a61878d00367f100
lenmei233 <1663996167@qq.com>
2026-07-07 21:53:14 +0800

    feat: integrate RunningProvider, LaunchProgressDialog, and notify bridge

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,17 @@ import AccountDetail from './pages/AccountDetail.tsx'
 import ResourceCenter from './pages/ResourceCenter.tsx'
 import ResourceDetailPage from './pages/ResourceDetail.tsx'
 import Settings from './pages/Settings.tsx'
-import { MessageBoxProvider } from './components/ui/message-box.tsx'
+import { MessageBoxProvider, useMessageBox } from './components/ui/message-box.tsx'
 import { loadSettings, onSettingsChange } from './api/settings.ts'
+import { RunningProvider, useRunning } from './contexts/RunningContext.tsx'
+import LaunchProgressDialog from './components/LaunchProgressDialog.tsx'
+
+function RunningNotifyBridge() {
+  const { notify } = useMessageBox()
+  const { setNotifyImpl } = useRunning()
+  useEffect(() => { setNotifyImpl(notify) }, [notify, setNotifyImpl])
+  return null
+}
 
 function App() {
   useEffect(() => {
@@ -27,23 +36,27 @@ function App() {
   }, [])
 
   return (
-    <MessageBoxProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/instances" element={<Instances />} />
-            <Route path="/instances/:id" element={<InstanceDetailPage />} />
-            <Route path="/downloads" element={<DownloadCenter />} />
-            <Route path="/accounts" element={<Accounts />} />
-            <Route path="/accounts/:uuid" element={<AccountDetail />} />
-            <Route path="/resource-center" element={<ResourceCenter />} />
-            <Route path="/resource-center/:resourceId" element={<ResourceDetailPage />} />
-            <Route path="/settings" element={<Settings />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </MessageBoxProvider>
+    <RunningProvider>
+      <MessageBoxProvider>
+        <BrowserRouter>
+          <RunningNotifyBridge />
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/instances" element={<Instances />} />
+              <Route path="/instances/:id" element={<InstanceDetailPage />} />
+              <Route path="/downloads" element={<DownloadCenter />} />
+              <Route path="/accounts" element={<Accounts />} />
+              <Route path="/accounts/:uuid" element={<AccountDetail />} />
+              <Route path="/resource-center" element={<ResourceCenter />} />
+              <Route path="/resource-center/:resourceId" element={<ResourceDetailPage />} />
+              <Route path="/settings" element={<Settings />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+        <LaunchProgressDialog />
+      </MessageBoxProvider>
+    </RunningProvider>
   )
 }
 
